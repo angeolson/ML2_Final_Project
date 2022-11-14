816e038c4db93e6bb2e2a2f1db8d3cf9cda12c34
angeolson <78054238+angeolson@users.noreply.github.com>
2022-11-14 12:20:19 -0500

    Create AOlson_Code_preprocess-images.py

---
+++ Code/AOlson_Code_preprocess-images.py
@@ -0,0 +1,36 @@
+# set global vars
+PATH = os.getcwd() + '/Code/Data/Vegetable Images'
+
+# create dataframes for train, val, and test data:
+train_dir = PATH + '/train'
+val_dir = PATH + '/validation'
+test_dir = PATH + '/test'
+
+def getFrame(filepath):
+    '''
+    :param filepath:
+    :return: dataframe of images and labels for the given split, encoded and converted to target
+    '''
+    mac_files = ['.DS_Store']
+    files = [file for file in os.listdir(filepath) if file not in mac_files]
+    df = pd.DataFrame()
+    for i in range(len(files)): # loop through each category
+        data = os.listdir(train_dir + "/" + files[i])
+        label = files[i]
+        data_dict = {'image': data, 'label_string': ([label]*len(data)), 'label': ([label]*len(data))}
+        frame = pd.DataFrame(data_dict)
+        df = df.append(frame)
+    encoded = pd.get_dummies(df, columns=['label'])
+    encoded['target'] = encoded.iloc[:, 2:].apply(list, axis=1)
+    return encoded
+
+# create splits and encode:
+train_df = getFrame(train_dir)
+val_df = getFrame(val_dir)
+test_df = getFrame(test_dir)
+
+# from inside class:
+label = self.data_frame.iloc[idx]['label_string']
+img_name = self.data_frame.iloc[idx]['image']
+img_path = os.path.join(self.root_dir, label, img_name)
+
