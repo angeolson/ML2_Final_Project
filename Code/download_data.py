4ef9db133095c0c4a6cc27530fe1504e4f5e3cb6
angeolson <78054238+angeolson@users.noreply.github.com>
2022-12-07 10:26:01 -0500

    Add new files for training, testing, downloading data

---
+++ Code/download_data.py
@@ -0,0 +1,18 @@
+# File to download and save the project data
+
+import os
+import gdown
+import argparse
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--path", default=None, type=str, required=True)  # Path of file
+# args = parser.parse_args()
+
+url = 'https://drive.google.com/u/3/uc?id=16U5WG2Jo8mU-J2jpo3PCnsvAn2CF2hF0&export=download'
+# PATH = args.path
+PATH ='/home/ubuntu/Final-Project-Group4'
+DATA_PATH = PATH + os.path.sep + 'Data'
+os.chdir(DATA_PATH)
+gdown.download(url, 'archive(4).zip', quiet=False)
+os.system("unzip 'archive(4).zip'")
+print('Done!')
