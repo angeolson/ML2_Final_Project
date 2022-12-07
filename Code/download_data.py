1290acbf8416b5acd2521fee46332074c0eea277
angeolson <78054238+angeolson@users.noreply.github.com>
2022-12-07 10:26:36 -0500

    Update download_data.py

--- Code/download_data.py
+++ Code/download_data.py
@@ -4,13 +4,13 @@ import os
 import gdown
 import argparse
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--path", default=None, type=str, required=True)  # Path of file
-# args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", default=None, type=str, required=True)  # Path of file
+args = parser.parse_args()
 
 url = 'https://drive.google.com/u/3/uc?id=16U5WG2Jo8mU-J2jpo3PCnsvAn2CF2hF0&export=download'
-# PATH = args.path
-PATH ='/home/ubuntu/Final-Project-Group4'
+PATH = args.path
+# PATH ='/home/ubuntu/Final-Project-Group4'
 DATA_PATH = PATH + os.path.sep + 'Data'
 os.chdir(DATA_PATH)
 gdown.download(url, 'archive(4).zip', quiet=False)
